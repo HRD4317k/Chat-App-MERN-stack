39099de1b3faaf8115c5b385ac4de455d3714f29
Rishabh Dehariya <rishabhdehariya88@gmail.com>
2025-06-30 03:34:52 +0530

    Project completed

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { THEMES } from "../constants";
 
 export const useThemeStore = create((set) => ({
   theme: localStorage.getItem("chat-theme") || "coffee",
